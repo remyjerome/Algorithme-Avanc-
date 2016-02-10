0bcbd7cc90fb3eb7605c892c06ec1e77c1dadefa
Remy Jerome <remy.jerome@live.com>
2016-02-10 20:08:48 +0100

    Tri QSort terminé

--- TP1/main.c
+++ TP1/main.c
@@ -13,12 +13,15 @@
 void afficheTableau(int * tab, int taille);
 void triFusion(int * tab,int p,int q);
 void fusionTableau(int * tab,int p,int q,int r);
+void quickSort(int * tableau, int debut, int fin);
+void echanger(int tableau[], int a, int b);
 
 int main(int argc, const char * argv[]) {
 
     int tab[] = {7,2,1,8,4,9,0,3,5,6};
     //afficheTableau(tab, 10);
-    triFusion(tab, 0, 9);
+    //triFusion(tab, 0, 9);
+    quickSort(tab, 0, 9);
     afficheTableau(tab, 10);
     return 0;
 }
@@ -84,5 +87,40 @@ void fusionTableau(int * tab,int p,int q,int r)
     }
 }
 
-
+void echanger(int tableau[], int a, int b)
+{
+    int temp = tableau[a];
+    tableau[a] = tableau[b];
+    tableau[b] = temp;
+}
+//Code openclassroom
+void quickSort(int * tableau, int debut, int fin)
+{
+    int gauche = debut-1;
+    int droite = fin+1;
+    const int pivot = tableau[debut];
+    
+    /* Si le tableau est de longueur nulle, il n'y a rien à faire. */
+    if(debut >= fin)
+        return;
+    
+    /* Sinon, on parcourt le tableau, une fois de droite à gauche, et une
+     autre de gauche à droite, à la recherche d'éléments mal placés,
+     que l'on permute. Si les deux parcours se croisent, on arrête. */
+    while(1)
+    {
+        do droite--; while(tableau[droite] > pivot);
+        do gauche++; while(tableau[gauche] < pivot);
+        
+        if(gauche < droite)
+            echanger(tableau, gauche, droite);
+        else break;
+    }
+    
+    /* Maintenant, tous les éléments inférieurs au pivot sont avant ceux
+     supérieurs au pivot. On a donc deux groupes de cases à trier. On utilise
+     pour cela... la méthode quickSort elle-même ! */
+    quickSort(tableau, debut, droite);
+    quickSort(tableau, droite+1, fin);
+}
 
